7e6a7e41e5a61338535ff57fdd4673dedf1acba8
Josh Sherman <josh@gravityblvd.com>
2014-01-17 12:04:05 -0500

    Updated PICKLES path

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-set_include_path(get_include_path() . PATH_SEPARATOR . '/usr/share/pickles-latest');
+set_include_path(get_include_path() . PATH_SEPARATOR . '/usr/share/pickles-v13.12.1');
 
 require_once 'pickles.php';
 
